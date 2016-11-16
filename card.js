e1d28c3b022ca1bde84ca3b4176e1ff7c94d1b1e
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-11-16 05:51:47 -0700

    Created card.js

---
+++ card.js
@@ -0,0 +1,11 @@
+/* Card Object */
+
+var Card = function (name, value, suit, title, color) {
+    this.name = name;
+    this.value = value;
+    this.suit =  suit;
+    this.title = title;
+    this.color = color;
+};
+
+/* Card Object Methods */
